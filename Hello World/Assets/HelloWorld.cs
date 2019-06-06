9db5732943ec6690bbbc132bb91e880a65d6a5a9
Sani Ahamed <thesua711@gmail.com>
2019-06-06 23:56:30 +0600

    innit

---
+++ Hello World/Assets/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        print("Hello! Me is Sani!");
+;	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
