18bb262a805fcd40895da7ea3214786f20709129
Sani Ahamed <thesua711@gmail.com>
2019-06-07 23:21:38 +0600

    Number Wizard, Respond to player input

--- Hello World/Assets/HelloWorld.cs
+++ Hello World/Assets/HelloWorld.cs
@@ -7,11 +7,33 @@ public class HelloWorld : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-        print("Helu! Sani!? ");
-;	}
+        int max = 1000,min = 1;
+          
+        Debug.Log("Welcome to number wizard!");
+        Debug.Log("Pink an number,don't tell me what it is..");
+        Debug.Log("The highest number you can pick is: "+max);
+        Debug.Log("The lowest number you can pick is: "+min);
+        Debug.Log("Tell me if your number is higher or lower then 500");
+        Debug.Log("Push Up = higher, Push Down = lower, Push Enter = Correct");
+        //print("Helu! Sani!? ");
+        ;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        { 
+            Debug.Log("Up Arrow key was pressed.");
+        }
+
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down Arror key was pressed");
+        }
+
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("You hit enter.");
+        }
+    }
 }
